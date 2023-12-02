0912b91ca270c6909ea31a3c0df3af9a2878511f
shamim2810 <shamim149120@gmail.com>
2023-12-02 21:01:25 +0600

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,14 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,43 +20,67 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _views = <Widget>[
+    const Center(
+      child: Text('Home is selected',
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.deepOrange,
+        ),
+      ),
+    ),
+    const Center(
+      child: Text('Home is Chat',
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.blue,
+        ),
+      ),
+    ),
+    const Center(
+      child: Text('Home is Setting',
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.pinkAccent,
+        ),
+      ),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Layout Builder'),
+        title: Text('Bottom Navigation Bar'),
         centerTitle: true,
         backgroundColor: Colors.teal,
       ),
-      
-      body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: RichText(
-                text: TextSpan(
-                  text: 'Do you have account?',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'Sign In!',
-                      style: TextStyle(
-                        color: Colors.pink,
-                        fontSize: 25,
-                        fontStyle: FontStyle.italic,
-                      )
-                    )
-                  ]
-                ),
 
-            ),
-          )
+      body: SafeArea(
+          child: _views.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.deepOrangeAccent,),label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.chat,color: Colors.blue,),label: 'Chat'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings,color: Colors.pink,),label: 'Setting'),
+          ],
+        currentIndex: _selectedIndex,
+        onTap: (index){
+            setState(() {
+              _selectedIndex = index;
+            });
+        },
       ),
     );
   }
