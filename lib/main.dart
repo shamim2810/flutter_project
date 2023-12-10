ae2263ed3c0892082c3f8ee02377475c02a8c691
shamim2810 <shamim149120@gmail.com>
2023-12-11 00:19:15 +0600

    SharedPreferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main(){
   runApp(MyApp());
@@ -29,27 +30,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String? dropDownValue;
+
+
+  late int _counter = 0;
+
+  void _increaseCounter() async{
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter++;
+    });
+    prefs.setInt('Counter', _counter);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadCounterFromSharedPreference();
+  }
+
+  void loadCounterFromSharedPreference()async{
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _counter = prefs.getInt('Counter') ?? 0;
+    });
+  }
+
+  void removeCounterFromSharedPrference()async{
+    final prefs = await SharedPreferences.getInstance();
+    
+    final success = await prefs.remove('Counter');
+    
+    print(success);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Floating Action Button'),
+      appBar: AppBar(title: Text('SharedPreferences'),
         backgroundColor: Colors.teal,
         centerTitle: true,
       ),
-      body: SafeArea(
-          child: Center()
+      body:SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Counter value is:- $_counter'),
+                SizedBox(height: 30,),
+                ElevatedButton(
+                    onPressed: _increaseCounter,
+                    child: Text('Click here')),
+                SizedBox(height: 20,),
+                ElevatedButton(
+                    onPressed: removeCounterFromSharedPrference,
+                    child: Text('Delete')),
+              ],
+            ),
+          )
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: Colors.teal,
-          onPressed: (){
-
-          },
-          label: Text('Add',style: TextStyle(color: Colors.white),),
-          icon: Icon(Icons.add,color: Colors.white,),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
